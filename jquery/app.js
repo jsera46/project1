35ff62a87b40e5e4d306dd7de4398387b5a27b57
jsera46 <jsera46@gmail.com>
2020-12-10 11:05:20 +0800

    praktis

---
+++ jquery/app.js
@@ -0,0 +1,35 @@
+
+
+$(document).ready(function(e){
+    // this for animated/animate.css
+    // $('.jumbotron').addClass('animate__animated animate__zoomInUp');
+    // $('button').addClass('animate__animated animate__bounceInDown animate__delay-1s');
+    // $('#target1').addClass('animate__animated animate__rollOut animate__delay-2s');
+
+    // $('#target2').addClass('btn-primary');
+    // $('#target3').css('color', 'red');
+
+    // // $('#target6').prop('disabled', true);
+    // $('#target6').prop('disabled', false);
+
+    // $('h2').html('<h1><i>This is Awesome Column</i></h1>');
+    
+    // $('#target2').appendTo('.right');
+
+    // $('#target3').remove();
+    // $('.right').clone().appendTo('.left');
+
+    // $('.row').parent().css('background-color', 'green');
+
+    // $('.container').children().css('background-color', '#000000');
+    // $('.row').children().css('background-color', '#f6f6f6');
+
+    // $('.target:nth-child(3)').addClass('animate__animated animate__rollIn');
+
+    // $('.target:even').addClass('animate__animated animate__rollIn');
+    $('.goodbye').addClass('animate__animated animate__hinge');
+
+
+
+
+});
